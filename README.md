27639d4e92af7a99055a94e6b4e4346b4b56d919
Mike Situ <michael.o.situ@gmail.com>
2013-10-22 19:03:01 -0700

    update readme

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ subs-zero
 =========
 
 Web app to display subreddit data
+
+Note: must use the h2s/reddit-api-client library and the latest version of PHP.
+
+Reddit API Client Install Instructions:
+```shell
+git clone https://github.com/h2s/reddit-console.git
+cd reddit-console
+php composer.phar install
+```
--- php/reddit.php
+++ php/reddit.php
@@ -1,5 +1,32 @@
 <?php
 
+require 'reddit-console/vendor/autoload.php';
+
+$reddit = new RedditApiClient\Reddit;
+$limit = 10;
+$top10links = $reddit->getLinksBySubreddit('funny', $limit); #default limit is 25
+
+// $toplink = $top10links[0];
+// printSummary($toplink);
+
+foreach($top10links as $l) {
+	printSummary($l);
+}
+
+function printSummary($link) {
+	echo nl2br("Title: " . $link->getTitle() . "\n");
+	echo nl2br("Score: " . $link->getScore() . "\n");
+	echo nl2br("URL: " . $link->getURL() . "\n");
+	echo nl2br("Author: " . $link->getAuthorName() . "\n\n");
+	// echo nl2br("Upvotes: " . $link->getUpvotes() . "\n");	
+	// echo nl2br("Downvotes: " . $link->getDownvotes() . "\n");	
+	// echo nl2br("ID: " . $link->getId() . "\n");
+	// echo nl2br("# Comments: " . $link->countComments() . "\n");
+	// echo nl2br("Self Text: " . $link->getSelfText() . "\n");
+	// echo nl2br("Permalink: " . $link->getPermaLink() . "\n");
+	// echo nl2br("Comments: " . $link->getComments() . "\n\n");
+}
+
 // SAMPLE CODE
 // require 'reddit-console/vendor/autoload.php';
 
@@ -42,30 +69,3 @@
 //         showComment($reply, $level + 1);
 //     }
 // }
-
-require 'reddit-console/vendor/autoload.php';
-
-$reddit = new RedditApiClient\Reddit;
-$limit = 10;
-$top10links = $reddit->getLinksBySubreddit('funny', $limit); #default limit is 25
-
-// $toplink = $top10links[0];
-// printSummary($toplink);
-
-foreach($top10links as $l) {
-	printSummary($l);
-}
-
-function printSummary($link) {
-	echo nl2br("Title: " . $link->getTitle() . "\n");
-	echo nl2br("Score: " . $link->getScore() . "\n");
-	echo nl2br("URL: " . $link->getURL() . "\n");
-	echo nl2br("Author: " . $link->getAuthorName() . "\n\n");
-	// echo nl2br("Upvotes: " . $link->getUpvotes() . "\n");	
-	// echo nl2br("Downvotes: " . $link->getDownvotes() . "\n");	
-	// echo nl2br("ID: " . $link->getId() . "\n");
-	// echo nl2br("# Comments: " . $link->countComments() . "\n");
-	// echo nl2br("Self Text: " . $link->getSelfText() . "\n");
-	// echo nl2br("Permalink: " . $link->getPermaLink() . "\n");
-	// echo nl2br("Comments: " . $link->getComments() . "\n\n");
-}
