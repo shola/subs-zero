da765565456a7a40ec0ecddb391b9fabc5cb5612
Mike Situ <michael.o.situ@gmail.com>
2013-10-30 12:06:07 -0700

    working out the bugs in php mail script

---
+++ php/mail/mail_settings.php
@@ -0,0 +1,8 @@
+<?php
+
+$email_to = "mail@omfg.skanzy.info"; // your email address send TO
+$email_from = "test@omfg.skanzy.info"; // your email address send FROM
+$email_subject = "Contact form message"; 
+$thankyou = "http://www.mikesitu.com"; 
+
+?>
